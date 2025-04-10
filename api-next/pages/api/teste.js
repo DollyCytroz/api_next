5c924a0989d1101d88483dbff817491284d44e3f
DollyCytroz <kauemcarreiro11@gmail.com>
2025-04-09 22:44:00 -0300

    ApiNext_Alunos

--- api-next/pages/api/teste.js
+++ api-next/pages/api/teste.js
@@ -1,7 +1,25 @@
-export default function handler(req, res){
-    if (req.method === 'GET'){
-        res.status(200).json({message:'pinto'})
-    } else {
-        res.status(405).json({message:'Não tem pinto'})
-    }
+import mysql from 'mysql2';
+ 
+// conexao com o banco
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'bdEscola'
+});
+ 
+// O que sera retornado na rota /api/teste
+export default function handle(req, res) {
+    /* Query que retorna um erro e resultado
+    e roda uma função para lidar com eles*/
+    connection.query('SELECT * FROM tbaluno', (error, results) => {
+        // Lidando com o erro
+        if (error) {
+            console.error('Erro na consulta', error);
+            return res.status(500).json({error: 'Erro ao buscar dados'});
+        }
+        // Lidando com o resultado
+        res.status(200).json(results);
+    })
 }
+ 
